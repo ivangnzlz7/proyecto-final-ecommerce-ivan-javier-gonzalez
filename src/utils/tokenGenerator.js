5fcd6428711045ef2ae512a8c0763269474d5aa7
Ivan javier <ig6119838@gmail.com>
2025-06-17 21:52:21 -0300

    expired hours

--- src/utils/tokenGenerator.js
+++ src/utils/tokenGenerator.js
@@ -6,6 +6,6 @@ const secret_key = process.env.JWT_SECRET_KEY;
 // Generar token JWT
 export const generateToken = ({id, email}) => {
     const user = {id, email};
-    const expiration = { expiresIn: '1h' };
+    const expiration = { expiresIn: '3h' };
     return jwt.sign(user, secret_key, expiration);
 }
