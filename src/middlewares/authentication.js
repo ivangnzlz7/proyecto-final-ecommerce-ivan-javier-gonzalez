83c614ddb93ec80e934c3bfce7a1dbffb61ca1fa
Ivan javier <ig6119838@gmail.com>
2025-07-03 20:37:57 -0300

    authentication message

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -6,12 +6,12 @@ const secret_key = process.env.JWT_SECRET_KEY;
 // Verificar token JWT
 export const authentication = (req, res, next) => {
     try {
-        const token = req.headers['authorization'].split(" ")[1] || null;
+        const token = req.headers['authorization'].split(" ")[1];
         jwt.verify(token, secret_key, (err) => {
             if(err) return res.status(403).json({message : 'Token invalido'});
             next();
         });
     } catch (error) {
-        res.status(401).json({message: 'Necesita un token'});
+        res.status(401).json({message: 'Necesita autorizacion'});
     }
 }; 
